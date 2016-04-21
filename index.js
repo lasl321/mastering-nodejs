cfdd3a874010d504a74cb1d4b6c269ac2b484fee
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-21 14:24:10 -0700

    Playing with process object

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-require.registerExtension('.ejs', require('./compiler/extended').compile);
+var u = { foo: 'bar' };
 
-var example = require('./compiler/example');
-console.dir(example)
-console.log(example.min(2, 3));
-console.log(example.max(10, 8));
+console.log(u);
+console.dir(u);
+
+console.assert(true, 'foo bar');
+
+// console.log();
+console.log(process.version);
+console.log(process.installPrefix);
+console.log(process.execPath);
+console.log(process.platform);
+console.log(process.pid);
+console.log(process.cwd());
+// console.log(process.chdir());
+var args = process.argv;
+console.log(args, args.length);
+
+process.on('uncaughtException', function(err){
+    console.log('got an error: %s', err.message);
+    process.exit(1);
+});
+
+setTimeout(function(){
+    throw new Error('fail');
+}, 100);
+
