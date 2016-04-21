2fd49adaa9320128f4669d7809e91acd744a797b
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-21 15:10:06 -0700

    Server with sys pump

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var http = require('http');
+var http = require('http'),
+    sys = require('sys');
 
 http.createServer(function(req, res){
     res.writeHead(200);
-    req.on('data', function (data) {
-        console.log('data', data, data.toString());
-        res.write(data);
-    });
-    req.on('end', function(){
-        console.log('end', arguments);
-        res.end();
-    });
+    sys.pump(req, res);
 }).listen(3000);
