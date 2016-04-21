d5d105f2a2c921852050b4ebd075a5d81c1e25b1
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-21 15:07:02 -0700

    First server (echo service)

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-var buf = new Buffer('…');
-console.log(buf.toString());
-// => …
+var http = require('http');
 
-var buf = new Buffer('…', 'ascii');
-console.log(buf.toString());
-// => &
-
-var hello = new Buffer([0x48, 0x65, 0x6c, 0x6c, 0x6f]);
-console.log(hello);
-console.log(hello.toString());
-
-var buf = new Buffer(5);
-buf.write('He');
-buf.write('l', 2);
-buf.write('lo', 3);
-console.log(buf.toString());
-// => "Hello"
-
-
-var ellipsis = new Buffer('…', 'utf8');
-
-console.log('… string length: %d', '…'.length);
-// => … string length: 1
-
-console.log('… byte length: %d', ellipsis.length);
-// => … byte length: 3
-
-console.log(ellipsis);
-// => <Buffer e2 80 a6>
+http.createServer(function(req, res){
+    res.writeHead(200);
+    req.on('data', function (data) {
+        console.log('data', data, data.toString());
+        res.write(data);
+    });
+    req.on('end', function(){
+        console.log('end', arguments);
+        res.end();
+    });
+}).listen(3000);
